0476ba84ad8e2b0657b32c82d6a514f7e4044368
Scott J. Krieder <skrieder@iit.edu>
2013-09-13 16:28:24 -0500

    auto test for python

--- python2.sh
+++ python2.sh
@@ -1,6 +1,30 @@
-ADLB_SERVERS=1 TURBINE_ENGINES=1 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 32 b1.tcl -N=250000
-ADLB_SERVERS=2 TURBINE_ENGINES=2 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 64 b1.tcl -N=250000
-ADLB_SERVERS=4 TURBINE_ENGINES=4 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 128 b1.tcl -N=250000
-ADLB_SERVERS=8 TURBINE_ENGINES=8 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 256 b1.tcl -N=250000
-ADLB_SERVERS=16 TURBINE_ENGINES=16 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 512 b1.tcl -N=250000
-ADLB_SERVERS=32 TURBINE_ENGINES=32 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 1024 b1.tcl -N=250000
+NODES=1
+N=1000000
+PPN=32
+AS=$NODES
+TE=$NODES
+nprocs=$(($PPN*$NODES))
+for i in {1..3}
+do
+    echo "N:" $N
+    echo "PPN:" $PPN
+    echo "NODES:" $NODES
+    echo "ADLB_SERVERS:" $AS
+    echo "TUBINE_ENGINES:" $TE
+    echo "nprocs:" $nprocs
+
+    ADLB_SERVERS=$AS TURBINE_ENGINES=$TE QUEUE=normal BLUE_WATERS=true PPN=$PPN turbine-aprun-run.zsh -n $nprocs b1.tcl -N=$N
+    
+    NODES=$(($NODES*2))
+    N=$(($N*2))
+    PPN=32
+    AS=$NODES
+    TE=$NODES
+    nprocs=$(($PPN*$NODES))
+done
+
+#ADLB_SERVERS=2 TURBINE_ENGINES=2 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 64 b1.tcl -N=250000
+#ADLB_SERVERS=4 TURBINE_ENGINES=4 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 128 b1.tcl -N=250000
+#ADLB_SERVERS=8 TURBINE_ENGINES=8 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 256 b1.tcl -N=250000
+#ADLB_SERVERS=16 TURBINE_ENGINES=16 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 512 b1.tcl -N=250000
+#ADLB_SERVERS=32 TURBINE_ENGINES=32 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 1024 b1.tcl -N=250000
--- sleep4.sh
+++ sleep4.sh
@@ -1,6 +1,6 @@
 PPN=32
-NODES=4
-bound=4000
+NODES=$1
+bound=$(($NODES * 2000))
 n=$(($PPN * $NODES))
 
 echo "PPN:" $PPN
@@ -8,7 +8,7 @@ echo "NODES:" $NODES
 echo "bound:" $bound
 echo "nprocs:" $n
 
-#TURBINE_ENGINES=$NODES ADLB_SERVERS=$NODES QUEUE=normal BLUE_WATERS=true PPN=$PPN turbine-aprun-run.zsh -n $n 589-big-loop.tcl -bound=$bound -sleeptime=1
+TURBINE_ENGINES=$NODES ADLB_SERVERS=$NODES QUEUE=normal BLUE_WATERS=true PPN=$PPN turbine-aprun-run.zsh -n $n 589-big-loop.tcl -bound=$bound -sleeptime=1
 TURBINE_ENGINES=$NODES ADLB_SERVERS=$NODES QUEUE=normal BLUE_WATERS=true PPN=$PPN turbine-aprun-run.zsh -n $n 589-big-loop.tcl -bound=$bound -sleeptime=0.1
 TURBINE_ENGINES=$NODES ADLB_SERVERS=$NODES QUEUE=normal BLUE_WATERS=true PPN=$PPN turbine-aprun-run.zsh -n $n 589-big-loop.tcl -bound=$bound -sleeptime=0.01
 TURBINE_ENGINES=$NODES ADLB_SERVERS=$NODES QUEUE=normal BLUE_WATERS=true PPN=$PPN turbine-aprun-run.zsh -n $n 589-big-loop.tcl -bound=$bound -sleeptime=0.001
