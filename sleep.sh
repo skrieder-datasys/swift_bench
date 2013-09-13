a87bd1aa7828dc0fec55c47620a1c38e67ba44c5
Scott J. Krieder <skrieder@iit.edu>
2013-09-13 15:30:33 -0500

    auto sleeps

--- sleep.sh
+++ sleep.sh
@@ -1,5 +1,15 @@
-TURBINE_ENGINES=1 ADLB_SERVERS=1 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 32 589-big-loop.tcl -bound=1000 -sleeptime=1
-TURBINE_ENGINES=1 ADLB_SERVERS=1 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 32 589-big-loop.tcl -bound=1000 -sleeptime=0.1
-TURBINE_ENGINES=1 ADLB_SERVERS=1 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 32 589-big-loop.tcl -bound=1000 -sleeptime=0.01
-TURBINE_ENGINES=1 ADLB_SERVERS=1 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 32 589-big-loop.tcl -bound=1000 -sleeptime=0.001
-TURBINE_ENGINES=1 ADLB_SERVERS=1 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 32 589-big-loop.tcl -bound=1000 -sleeptime=0
+PPN=32
+NODES=$1
+bound=$(($NODES * 2000))
+n=$(($PPN * $NODES))
+
+echo "PPN:" $PPN
+echo "NODES:" $NODES
+echo "bound:" $bound
+echo "nprocs:" $n
+
+TURBINE_ENGINES=$NODES ADLB_SERVERS=$NODES QUEUE=normal BLUE_WATERS=true PPN=$PPN turbine-aprun-run.zsh -n $n 589-big-loop.tcl -bound=$bound -sleeptime=1
+TURBINE_ENGINES=$NODES ADLB_SERVERS=$NODES QUEUE=normal BLUE_WATERS=true PPN=$PPN turbine-aprun-run.zsh -n $n 589-big-loop.tcl -bound=$bound -sleeptime=0.1
+TURBINE_ENGINES=$NODES ADLB_SERVERS=$NODES QUEUE=normal BLUE_WATERS=true PPN=$PPN turbine-aprun-run.zsh -n $n 589-big-loop.tcl -bound=$bound -sleeptime=0.01
+TURBINE_ENGINES=$NODES ADLB_SERVERS=$NODES QUEUE=normal BLUE_WATERS=true PPN=$PPN turbine-aprun-run.zsh -n $n 589-big-loop.tcl -bound=$bound -sleeptime=0.001
+TURBINE_ENGINES=$NODES ADLB_SERVERS=$NODES QUEUE=normal BLUE_WATERS=true PPN=$PPN turbine-aprun-run.zsh -n $n 589-big-loop.tcl -bound=$bound -sleeptime=0
