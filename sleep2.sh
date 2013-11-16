e43e55974efaf070db34d7b9d9d43a5c66a60875
Scott J. Krieder <skrieder@iit.edu>
2013-11-16 17:58:10 -0600

    updating to take args

--- sleep2.sh
+++
@@ -1,5 +0,0 @@
-TURBINE_ENGINES=2 ADLB_SERVERS=2 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 64 589-big-loop.tcl -bound=4000 -sleeptime=1
-TURBINE_ENGINES=2 ADLB_SERVERS=2 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 64 589-big-loop.tcl -bound=4000 -sleeptime=0.1
-TURBINE_ENGINES=2 ADLB_SERVERS=2 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 64 589-big-loop.tcl -bound=4000 -sleeptime=0.01
-TURBINE_ENGINES=2 ADLB_SERVERS=2 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 64 589-big-loop.tcl -bound=4000 -sleeptime=0.001
-TURBINE_ENGINES=2 ADLB_SERVERS=2 QUEUE=normal BLUE_WATERS=true PPN=32 turbine-aprun-run.zsh -n 64 589-big-loop.tcl -bound=4000 -sleeptime=0
